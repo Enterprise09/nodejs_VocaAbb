7cf7b084d0298e2ac16aedc968fd9b5a7eb2d127
Enterprise09 <goldenstaytes@gmail.com>
2021-01-26 23:09:34 +0900

    Complete-Nodejs-Lesson22

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const server = express();
+let words = require("./db/words.json");
+
+server.engine("hbs", hbs({
+    extname: "hbs",
+    defaultLayout: "layout.hbs",
+    partialsDir: "partials",
+})
+);
+
+server.set("view engine", "hbs");
+server.use(express.static(__dirname + "/public"));
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.get("/", (req, res) => {
+    res.render("home", {
+        words,
+    });
+    console.log("request URL: " + req.url);
+});
+
+server.post("/", (req, res) => {
+    const { query } = req.body;
+    res.render("home", {
+        words: words.filter((w) => 
+            w.word.toLowerCase().includes(query.toLowerCase())
+        ),    
+    });
+});
+
+server.delete("/", (req, res) => {
+    let { word } = req.body;
+    words = words.filter((w) => !(w.word === word));
+});
+
+server.get("/add", (req, res) => {
+    res.render("add");
+    console.log("request URL: " + req.url);
+});
+
+server.get("/quiz", (req, res) => {
+    res.render("quiz");
+    console.log("request URL: " + req.url);
+});
+
+server.use((req, res) => {
+    res.render("404");
+});
+
+server.listen(3000, (err) => {
+    if (err) return console.log(err);
+    console.log("The server is listening on port 3000");
+});
+
+
+
